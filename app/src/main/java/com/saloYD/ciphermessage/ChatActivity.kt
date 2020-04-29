27d55aa80b7550378fa097443ce9c61858a08074
m-tkachenko <tk.mykhailo@gmail.com>
2020-04-29 16:11:27 +0200

    ChatActivity: Encrypt Messages

--- app/src/main/java/com/saloYD/ciphermessage/ChatActivity.kt
+++ app/src/main/java/com/saloYD/ciphermessage/ChatActivity.kt
@@ -1,9 +1,13 @@
 package com.saloYD.ciphermessage
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
@@ -16,11 +20,38 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.alert_dialog_cipher_key.*
+import kotlinx.android.synthetic.main.alert_dialog_cipher_key.view.*
 import kotlinx.android.synthetic.main.users_row_chat_from.view.*
 import kotlinx.android.synthetic.main.users_row_chat_to.view.*
 
 
 class ChatActivity : AppCompatActivity() {
+
+    fun encrypt(s: String, key: Int): String {
+        val offset = key % 26
+        if (offset == 0) return s
+        var d: Char
+        val chars = CharArray(s.length)
+        for ((index, c) in s.withIndex()) {
+            if (c in 'A'..'Z') {
+                d = c + offset
+                if (d > 'Z') d -= 26
+            }
+            else if (c in 'a'..'z') {
+                d = c + offset
+                if (d > 'z') d -= 26
+            }
+            else
+                d = c
+            chars[index] = d
+        }
+        return chars.joinToString("")
+    }
+
+    fun decrypt(s: String, key: Int): String {
+        return encrypt(s, 26 - key)
+    }
 
     companion object {
         val TAG = "ChatActivity"
@@ -52,6 +83,13 @@ class ChatActivity : AppCompatActivity() {
 
             Log.d(TAG, "Atempt to send message")
             doToSendMessage()
+        }
+
+        new_message_button.setOnLongClickListener {
+
+            doEncryptMessages()
+
+            return@setOnLongClickListener false
         }
     }
 
@@ -91,18 +129,62 @@ class ChatActivity : AppCompatActivity() {
         })
     }
 
-    private fun doToSendMessage() {
+    private fun alertCipherDialog() {
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Enter cipher key")
+
+        builder.setPositiveButton("OK", DialogInterface.OnClickListener{dialog, which ->
+            dialog.dismiss()
+            val encryptKey = edittext_key_cipher.text.toString().toInt()
+        })
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+
+    }
+
+    private fun doEncryptMessages() {
 
         val user = intent.getParcelableExtra<User>(NewMessageActivity.USER_KEY)
 
-        val messageText = new_message_edittext.text.toString()
+        var messageText = new_message_edittext.text.toString()
+        val encryptMessage = encrypt(messageText, 3)
+        messageText = encryptMessage
+
         val fromId = FirebaseAuth.getInstance().uid ?: return
         val toId = user.userUid
 
         val fromReference = FirebaseDatabase.getInstance().getReference("/user-messages/$fromId/$toId").push()
         val toReference = FirebaseDatabase.getInstance().getReference("/user-messages/$toId/$fromId").push()
 
-        val chatMessage = ChatMessage(fromReference.key!!, messageText,  fromId, toId, System.currentTimeMillis() / 1000)
+        val chatMessage = ChatMessage(fromReference.key!!, messageText, fromId, toId, System.currentTimeMillis() / 1000)
+
+        fromReference.setValue(chatMessage)
+            .addOnSuccessListener {
+
+                Log.d(TAG, "Message saved to firebase: ${fromReference.key}")
+                new_message_edittext.text.clear()
+                recyclerview_chat.scrollToPosition(adapter.itemCount - 1)
+            }
+
+        toReference.setValue(chatMessage)
+    }
+
+
+    private fun doToSendMessage() {
+
+        val user = intent.getParcelableExtra<User>(NewMessageActivity.USER_KEY)
+
+        val messageText = new_message_edittext.text.toString()
+
+        val fromId = FirebaseAuth.getInstance().uid ?: return
+        val toId = user.userUid
+
+        val fromReference = FirebaseDatabase.getInstance().getReference("/user-messages/$fromId/$toId").push()
+        val toReference = FirebaseDatabase.getInstance().getReference("/user-messages/$toId/$fromId").push()
+
+        val chatMessage = ChatMessage(fromReference.key!!, messageText, fromId, toId, System.currentTimeMillis() / 1000)
 
         fromReference.setValue(chatMessage)
             .addOnSuccessListener {
@@ -121,7 +203,6 @@ class ChatActivity : AppCompatActivity() {
         latestMessageToRef.setValue(chatMessage)
     }
 }
-
 
 class ChatFromItem(val userText : String, val user : User) : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
